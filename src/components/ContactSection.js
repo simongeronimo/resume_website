c60394a3bbd6800763d8823400d456cf8964bd3b
simongeronimo <simongeronimo>
2023-03-19 22:54:22 -0400

    Added Captcha with Hidden badge and added success message and form reset

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,8 +1,35 @@
 import { Fragment } from "react";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { send } from 'emailjs-com';
 
+
 const ContactSection = () => {
+  const [status, setStatus] = useState(undefined);
+  const SITE_KEY = "6Le1bBIlAAAAAGHDOr2BKvIZBeY6ChyGWq3jVgUo";
+  
+  useEffect(() => {
+  const loadScriptByURL = (id, url, callback) => {
+    const isScriptExist = document.getElementById(id);
+ 
+    if (!isScriptExist) {
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = url;
+      script.id = id;
+      script.onload = function () {
+        if (callback) callback();
+      };
+      document.body.appendChild(script);
+    }
+ 
+    if (isScriptExist && callback) callback();
+  }
+ 
+  // load the script by passing the URL
+  loadScriptByURL("recaptcha-key", `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`, function () {
+    console.log("Script loaded!");
+  });
+}, []);
 
   const [toSend, setToSend] = useState({
     from_name: '',
@@ -12,18 +39,27 @@ const ContactSection = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    send(
+    window.grecaptcha.ready(() => {
+    window.grecaptcha.execute(SITE_KEY, { action: 'submit' }).then(token => {
+      send(
       'service_r076udj',
       'template_m0gbq3i',
       toSend,
       'spgCywRGDVSjAJbpK'
     )
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        setStatus({ type: 'success' });
+        setToSend('','','');
+        document.getElementById("formName").value='';
+        document.getElementById("formEmail").value='';
+        document.getElementById("formMessage").value='';
+        
       })
       .catch((err) => {
         console.log('FAILED...', err);
       });
+    });
+  });
   };
 
   const handleChange = (e) => {
@@ -76,15 +112,16 @@ const ContactSection = () => {
           </div>
           {/* form */}
           <div className="contact_form content-box">
-            <form id="cform" method="post">
+            <form id="cform" method="post" onSubmit={onSubmit}>
               <div className="group-val">
-                <input type="text" name="from_name" placeholder="Name" value={toSend.from_name} onChange={handleChange}/>
+                <input type="text" id="formName" name="from_name" placeholder="Name" value={toSend.from_name} onChange={handleChange}/>
               </div>
               <div className="group-val">
-                <input type="email" name="reply_to" placeholder="Email" value={toSend.reply_to} onChange={handleChange}/>
+                <input type="email" id="formEmail" name="reply_to" placeholder="Email" value={toSend.reply_to} onChange={handleChange}/>
               </div>
               <div className="group-val ct-gr">
                 <textarea
+                  id="formMessage"
                   name="message"
                   placeholder="Message"
                   defaultValue={""}
@@ -99,9 +136,14 @@ const ContactSection = () => {
                 </button>
               </div>
             </form>
-            <div className="alert-success">
-              <p>Thanks, your message is sent successfully.</p>
+            <div>
+              <p class="zz_code">This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
             </div>
+            {status?.type === 'success' &&             
+            <div>
+              <p>Thanks, your message has been sent successfully.</p>
+            </div>}
+
           </div>
         </div>
         <div className="clear" />
